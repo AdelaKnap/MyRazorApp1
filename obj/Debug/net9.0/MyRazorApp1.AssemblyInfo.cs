6ad4749ee6ac0ae2b21842e03ced6381d0b4e71f
Adela Knap <adkn2300@student.miun.se>
2025-01-21 12:00:17 +0100

    Testar igen...

--- obj/Debug/net9.0/MyRazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyRazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24655d08369f5f45b244b1eb9d2f9d08d1e85377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41db1f50b1e9b454eb56c2bb983b080fc17c10c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
